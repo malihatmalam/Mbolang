3bf6cf5f06ab235193657d5c3724c24706930107
malihatmalam <malihatmalam@gmail.com>
2020-10-31 18:54:16 +0700

    Initial Project Mbolang

---
+++ database/migrations/2020_10_31_053940_create_guides_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuidesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guides', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->date('birt');
+            $table->enum('gender',['Female','Male']);
+            $table->boolean('status');
+            $table->text('address');
+            $table->bigInteger('country');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guides');
+    }
+}
---
+++ database/migrations/2020_10_31_055509_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_guide');
+            $table->foreign('id_guide')->references('id')->on('guides');
+            $table->string('title');
+            $table->longText('detail');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
